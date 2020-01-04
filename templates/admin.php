f1708ceabcf8efb56876817e75eb7bb0e61e7e10
Anas <anas9719@gmail.com>
2020-01-04 17:53:20 +0530

    4jan2020-checkbox functinality complete

--- templates/admin.php
+++ templates/admin.php
@@ -4,8 +4,8 @@
 
     <ul class="nav nav-tabs">
         <li class="active"><a href="#tab-1">Manage Setting</a></li>
-        <li><a href="#tab-2">Updates</a></li>
-        <li><a href="#tab-3">About</a></li>
+        <!-- <li><a href="#tab-2">Updates</a></li>
+        <li><a href="#tab-3">About</a></li> -->
     </ul>
 
 
@@ -19,12 +19,12 @@
             ?>
             </form> 
         </div>
-        <div id="tab-2"class="tab-pane"> 
+        <!-- <div id="tab-2"class="tab-pane"> 
             <h3>Updates</h3>
         </div>
         <div id="tab-3"class="tab-pane"> 
             <h3>About</h3>
-        </div>
+        </div> -->
     </div>
 
     
